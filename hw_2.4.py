49b033d3627aa745dea1d4c121848f3c2d7a7ebb
AntonChernyshovDolgopa <chernyshow59784@mail.ru>
2017-07-04 22:13:50 +0300

    Add files via upload

---
+++ hw_2.4.py
@@ -0,0 +1,19 @@
+import os
+import io
+
+os.chdir(r'C:\Python\Задание 10\Python_course-master (2)\Python_course-master\homework\2.3-paths\Migrations')
+files_list = []
+for name in os.listdir("."):
+    if name.endswith(".sql"):
+    	files_list.append(name)
+
+list_with_word = []
+word  = input ()
+for file in files_list:
+	with io.open(file) as f:
+		for line in f:
+			if word in line:
+				list_with_word.append(file)
+				break
+
+print (len(set(list_with_word)))
